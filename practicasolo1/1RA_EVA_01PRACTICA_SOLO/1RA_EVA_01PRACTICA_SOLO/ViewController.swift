c858ac3e5195d5719df0e4fe2a7e7b3fdf301b5d
TUJEFA101 <rafa_rock_96@hotmail.com>
2017-02-17 10:33:23 -0700

    Add files via upload

---
+++ practicasolo1/1RA_EVA_01PRACTICA_SOLO/1RA_EVA_01PRACTICA_SOLO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  1RA_EVA_01PRACTICA_SOLO
+//
+//  Created by MIGUEL ANGEL on 15/02/17.
+//  Copyright © 2017 RAFAEL GRADO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
